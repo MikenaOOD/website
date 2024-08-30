7cc61ec571bb392e5e126a612a229ba4994c07d2
Viktor Tsolov <vt@auxcode.com>
2024-08-30 11:07:56 +0300

    add partial navigation

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Layout from "../components/Layout";
 
 const Page = ({ data }) => {
   if (!data) return null;
   const page = data.prismicPage;
 
   return (
-    <React.Fragment>
+    <Layout>
       <h1>{page.data.title.text}</h1>
       <div dangerouslySetInnerHTML={{ __html: page.data.content.html }} />
-    </React.Fragment>
+    </Layout>
   );
 };
 
